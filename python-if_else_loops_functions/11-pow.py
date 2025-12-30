93a0bf6deb7cec649c09c9dfb83e13609b90026d
oways-work <owayswork@gmail.com>
2025-12-30 06:00:56 +0300

    Create 11-pow.py

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a function to compute a to the power of b.
+"""
+
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+
+    Args:
+        a (int): The base number.
+        b (int): The exponent.
+
+    Returns:
+        int/float: The result of a ^ b.
+    """
+    return (a ** b)
