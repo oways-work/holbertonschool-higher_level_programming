d17e0deb21f99a16a5c9ac2ffe92c51c884949ae
oways-work <owayswork@gmail.com>
2026-01-04 02:39:06 +0300

    Update 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-          """Divides 2 integers and prints the result."""
-          try:
-                        result = a / b
-except (TypeError, ZeroDivisionError):
+    """
+    Divides 2 integers and prints the result.
+
+    Args:
+        a: The first integer (numerator).
+        b: The second integer (denominator).
+
+    Returns:
+        The value of the division, otherwise: None.
+    """
+    try:
+        result = a / b
+    except (ZeroDivisionError, TypeError):
         result = None
-finally:
+    finally:
         print("Inside result: {}".format(result))
-          return result
+    return result
