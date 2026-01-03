1423c098d70e974bd0f30b33f3cf6fe8b4a65cf4
oways-work <owayswork@gmail.com>
2026-01-03 07:48:01 +0300

    Create 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Add (0, 0) to ensure at least 2 elements, then slice first 2
+    a = (tuple_a + (0, 0))[:2]
+    b = (tuple_b + (0, 0))[:2]
+    
+    return (a[0] + b[0], a[1] + b[1])
