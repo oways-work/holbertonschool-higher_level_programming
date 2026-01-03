1ba496f986e3b91df493cc1c094ab6b1eff9c14c
oways-work <owayswork@gmail.com>
2026-01-03 10:18:37 +0300

    Update 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that multiplies values by 2."""
+
+
 def multiply_by_2(a_dictionary):
-          return {key: value * 2 for key, value in a_dictionary.items()}
+    """Returns a new dictionary with all values multiplied by 2."""
+    return {key: value * 2 for key, value in a_dictionary.items()}
