1fa68ec1d0869c142641761fa4d4824dfc15e8b0
oways-work <owayswork@gmail.com>
2026-01-04 02:36:20 +0300

    just push it

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     """
     Prints the first x elements of a list and only integers.
-    
+
     Args:
         my_list: The list to print from.
         x: The number of elements to access.
-        
+
     Returns:
         The real number of integers printed.
     """
