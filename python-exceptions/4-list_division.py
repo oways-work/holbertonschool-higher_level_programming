8b4942d63932c73815e5d89eba25a3bd0a4350a0
oways-work <owayswork@gmail.com>
2026-01-04 02:39:46 +0300

    Update 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-      """
-          Divides element by element 2 lists.
-              Returns a new list (length = list_length) with ions.
-                  """
-      new_list = []
-      for i in range(list_length):
-                try:
-                              result = 0
-                              result = my_list_1[i] / my_list_2[i]
-                except TypeError:
-                              print("wrong type")
-                              result = 0
-                except ZeroDivisionError:
-                              print("division by 0")
-                              result = 0
-                except IndexError:
-                              print("out of range")
-                              result = 0
-                finally:
-                              new_list.append(result)
-                      return new_listall divis
+    """
+    Divides element by element 2 lists.
+
+    Args:
+        my_list_1: First list.
+        my_list_2: Second list.
+        list_length: Number of elements to divide.
+
+    Returns:
+        A new list of length list_length with all divisions.
+    """
+    new_list = []
+    for i in range(list_length):
+        res = 0
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(res)
+    return new_list
