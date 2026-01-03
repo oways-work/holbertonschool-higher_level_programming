fd2f0b8e6db0880a60fadf88ea053b154c9b272c
oways-work <owayswork@gmail.com>
2026-01-03 10:17:58 +0300

    Update 7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Defines a function that updates a dictionary."""
+
+
 def update_dictionary(a_dictionary, key, value):
-          a_dictionary[key] = value
-          return a_dictionary
+    """Replaces or adds key/value in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
