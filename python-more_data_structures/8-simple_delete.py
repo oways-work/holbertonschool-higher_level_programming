9beecb794f229d61e0fc1f8780baa481b689bf10
oways-work <owayswork@gmail.com>
2026-01-03 10:18:18 +0300

    Update 8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a function that deletes a key."""
+
+
 def simple_delete(a_dictionary, key=""):
-          if key in a_dictionary:
-                        del a_dictionary[key]
-                    return a_dictionary
+    """Deletes a key in a dictionary."""
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
