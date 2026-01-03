7305ceb56731b1e85b952cbad2b5b2d6396f222f
oways-work <owayswork@gmail.com>
2026-01-03 08:38:49 +0300

    ready_from_note

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Defines a function that prints integers of a list."""
+
+
 def print_list_integer(my_list=[]):
+    """Prints all integers of a list."""
     for i in my_list:
         print("{:d}".format(i))
