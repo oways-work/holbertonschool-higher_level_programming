2d89ac178bbaee3e5c11d2e0a57114b7a0ecade9
oways-work <owayswork@gmail.com>
2026-01-03 10:15:27 +0300

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that replaces occurrences of an element."""
+
+
 def search_replace(my_list, search, replace):
+    """Replaces all occurrences of an element by another in a new list."""
+    return [replace if x == search else x for x in my_list]
