2231a72ff1e28dc0fd0a2e95d8cfa2d03c557127
oways-work <owayswork@gmail.com>
2026-01-03 10:18:56 +0300

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a function that returns the key with the biggest integer value."""
+
+
 def best_score(a_dictionary):
-          if not a_dictionary:
-                        return None
-                    return max(a_dictionary, key=a_dictionary.get)
+    """Returns a key with the biggest integer value."""
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
