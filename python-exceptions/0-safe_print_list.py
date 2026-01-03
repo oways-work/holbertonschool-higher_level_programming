13fda357f9fc3cfd8f775a85ee1437cd346f3c1e
oways-work <owayswork@gmail.com>
2026-01-04 02:32:36 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    """Prints the first x elements of a list and only integers."""
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements of a list.
+    """
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
-            continue
+        except IndexError:
+            break
     print("")
     return count
