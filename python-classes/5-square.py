a8ef2365e66dc24349903e720ded4d950a054fc5
oways-work <owayswork@gmail.com>
2026-01-04 09:27:06 +0300

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,42 +3,42 @@
 
 
 class Square:
-              """Represent a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-                      """Initialize a new Square.
+        """Initialize a new Square.
 
-                              Args:
-                                          size (int): The size of the new square.
-                                                  """
-                      self.size = size
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-                      """Get the current size of the square."""
-                      return (self.__size)
+        """Get the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-                      """Set the size of the square.
+        """Set the size of the square.
 
-                              Args:
-                                          value (int): The new size of the square.
-                                                  """
-                      if not isinstance(value, int):
-                                            raise TypeError("size must be an integer")
-                                        if value < 0:
-                                                              raise ValueError("size must be >= 0")
-                                                          self.__size = value
+        Args:
+            value (int): The new size of the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-                      """Return the current area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-                      """Print the square with the # character."""
+        """Print the square with the # character."""
         if self.size == 0:
-                              print("")
-else:
+            print("")
+        else:
             for i in range(self.size):
-                                      print("#" * self.size)
+                print("#" * self.size)
