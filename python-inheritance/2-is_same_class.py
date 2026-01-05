3a1c89c9850c0f82267ea90e214af9182ee3ab41
oways-work <owayswork@gmail.com>
2026-01-05 09:11:11 +0300

    Create 2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class-checking function."""
+
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match.
+    Returns:
+        True if exactly an instance, otherwise False.
+    """
+    return type(obj) is a_class
