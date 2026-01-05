4b7747aa3bc48ff31e0d80c6ad476a5f5f0ce46e
oways-work <owayswork@gmail.com>
2026-01-05 09:28:18 +0300

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,16 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an inherited instance (subclass only) of a class."""
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """Check if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        bool: True if obj is an instance of a class that inherited
+        (directly or indirectly) from a_class; otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
