1444e61cc1f5f951a5f8f534c9a7e5d3aafdbf7a
oways-work <owayswork@gmail.com>
2026-01-06 14:00:26 +0300

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""This module defines the BaseGeometry class."""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """A class that represents base geometry logic."""
+    """Represents base geometry."""
 
     def area(self):
-        """Raises an Exception because the area is not implemented yet."""
+        """Not implemented yet.
+
+        Raises:
+            Exception: always.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that a value is a positive integer."""
+        """Validates a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
