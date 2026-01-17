63c750be96eda803ee9d6b95f021ab5e7c1eb472
oways-work <owayswork@gmail.com>
2026-01-17 23:53:11 +0300

    Update 1-create_database_if_missing.sql

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
--- Creates the database hbtn_0c_0 in my MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Creates the MySQL server user user_0d_1 with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
