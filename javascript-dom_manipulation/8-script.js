1bff16023d8fdda48dacdd89683ec244aa0ec4dc
oways-work <owayswork@gmail.com>
2026-02-05 18:58:38 +0300

    Finalize JS DOM project: fix Task 8 URL and semistandard compliance

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,4 +1,4 @@
-const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+const url = 'https://hellosalut.stefanbohacek.com/?lang=fr';
 const helloDiv = document.querySelector('#hello');
 
 fetch(url)
